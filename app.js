42a3f3a6f0cd19251261a624e7047b747ab78cb1
Rudra-Prasad-Das <rudrarpd08@gmail.com>
2021-06-17 13:54:00 +0530

    add procfile gitignore and udated

--- app.js
+++ app.js
@@ -130,6 +130,6 @@ if(click===todayDate){
 
 })
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server started.");
 });
